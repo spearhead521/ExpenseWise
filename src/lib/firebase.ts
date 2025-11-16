504691bb31fb699b2ea34645b276ebc917f57822
GLBITM Prakhar <prakharglbitm@gmail.com>
2025-11-16 19:52:28 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "studio-4257982084-6f16b",
+  "appId": "1:66996488862:web:71e3425bea21b5703f498f",
+  "apiKey": "AIzaSyAbrNcMLH9rbjjkwcaVPNkxsVqtcSkg7M0",
+  "authDomain": "studio-4257982084-6f16b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "66996488862"
 };
 
 // Initialize Firebase
